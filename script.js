9e3c79e00007346a24d92dbfbe1be3a8dcaa7004
Saipavan8790 <127079433+Saipavan8790@users.noreply.github.com>
2023-10-28 08:29:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function liveTime() {
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    let time = date.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true });
+    let currentDate = `${month}/${day}/${year}`;
+    document.getElementById("timer").textContent = currentDate + ', ' + time;
+}
+
+function startTimer() {
+    liveTime();
+    setInterval(liveTime, 1000);
+}
+
+window.addEventListener('load', startTimer);
